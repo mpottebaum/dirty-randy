c17125366fd466e608cef511fac6f6f8cf6dd31a
Mike Pottebaum <michael.pottebaum@cdw.com>
2024-03-28 23:48:20 -0500

    lotta refacto

--- main.go
+++ main.go
@@ -47,14 +47,20 @@ var NewSel = cascadia.Parse
 var Query = cascadia.Query
 var QueryAll = cascadia.QueryAll
 
-func main() {
+func UserInput() (league string) {
 	args := os.Args
 	if len(args) <= 1 {
 		fmt.Println("Please enter a league name")
 		return
 	}
-	yourLeague := args[1]
-	url := "https://www.espn.com/" + yourLeague + "/schedule"
+	//league = args[0]
+	//no need to lie
+	league = "f1"
+	return
+}
+
+func GetScheduleHTML(league string) *html.Node {
+	url := "https://www.espn.com/" + league + "/schedule"
 	resp, err := http.Get(url)
 	check(err)
 	defer resp.Body.Close()
@@ -63,21 +69,87 @@ func main() {
 	check(err)
 	doc, err := html.Parse(strings.NewReader(string(content)))
 	check(err)
-	rowSel, err := NewSel("tr.Table__TR")
+	return doc
+}
+
+func BuildSels() (rowSel, raceSel, raceNameSel, raceTrackSel, lightsOutSel, dateTimeSel, tvSel cascadia.Sel) {
+	var err error
+	rowSel, err = NewSel("tr.Table__TR")
 	check(err)
-	raceSel, err := NewSel("td.race__col")
+	raceSel, err = NewSel("td.race__col")
 	check(err)
-	raceNameSel, err := NewSel("a")
+	raceNameSel, err = NewSel("a")
 	check(err)
-	raceTrackSel, err := NewSel("div")
+	raceTrackSel, err = NewSel("div")
 	check(err)
-	lightsOutSel, err := NewSel("td.winnerLightsOut__col")
+	lightsOutSel, err = NewSel("td.winnerLightsOut__col")
 	check(err)
-	dateTimeSel, err := NewSel("span")
+	dateTimeSel, err = NewSel("span")
 	check(err)
-	tvSel, err := NewSel("td.tv__col")
+	tvSel, err = NewSel("td.tv__col")
 	check(err)
-	rowsResult := QueryAll(doc, rowSel)
+	return
+}
+
+func GetRows(doc *html.Node, sel cascadia.Sel) []*html.Node {
+	return QueryAll(doc, sel)
+}
+
+func GetNameAndLocation(race *html.Node, nameSel cascadia.Sel, trackSel cascadia.Sel) (name string, location string) {
+	// name and location
+	raceNameResult := Query(race, nameSel)
+	raceTrackResult := Query(race, trackSel)
+	if raceNameResult.FirstChild != nil {
+		name = raceNameResult.FirstChild.Data
+	}
+	if raceTrackResult.FirstChild != nil {
+		location = raceTrackResult.FirstChild.Data
+	}
+	return
+}
+
+func GetDateAndTime(lightsOut *html.Node, dateTimeSel cascadia.Sel) (formattedDate string, formattedTime string) {
+	dateTimeResult := Query(lightsOut, dateTimeSel)
+	//if race in past, there's no span (just winner's name)
+	if dateTimeResult != nil && dateTimeResult.FirstChild != nil {
+		rawDateTimeStr := dateTimeResult.FirstChild.Data
+		fullDateAndTime := strings.Split(rawDateTimeStr, " - ")
+		fullDate := fullDateAndTime[0]
+		monthAndDay := strings.Split(fullDate, " ")
+		month := monthAndDay[0]
+		formattedMonth := MonthMap[month]
+		day := monthAndDay[1]
+		formattedDay := day
+		if len(day) == 1 {
+			formattedDay = "0" + formattedDay
+		}
+		eventTime := fullDateAndTime[1]
+		hourAndPeriod := strings.Split(eventTime, " ")
+		hour := hourAndPeriod[0]
+		period := hourAndPeriod[1]
+		formattedPeriod := strings.ToUpper(period)
+		year := strconv.Itoa(time.Now().Year())
+		formattedDate = formattedMonth + "/" + formattedDay + "/" + year
+		formattedTime = hour + " " + formattedPeriod
+	}
+	return
+}
+
+func main() {
+	//STEP ONE: GET LEAGUE NAME
+	//TODO: support other leagues
+	yourLeague := UserInput()
+	//STEP TWO: GET SCHEDULE HTML
+	doc := GetScheduleHTML(yourLeague)
+	//STEP THREE: PARSE HTML
+	rowSel,
+		raceSel,
+		raceNameSel,
+		raceTrackSel,
+		lightsOutSel,
+		dateTimeSel,
+		tvSel := BuildSels()
+	rowsResult := GetRows(doc, rowSel)
 	events := [][]string{}
 ParseRows:
 	for _, row := range rowsResult {
@@ -85,41 +157,15 @@ ParseRows:
 		// name and location
 		raceResult := Query(row, raceSel)
 		if raceResult != nil {
-			raceNameResult := Query(raceResult, raceNameSel)
-			raceTrackResult := Query(raceResult, raceTrackSel)
-			if raceNameResult.FirstChild != nil {
-				newEvent[0] = raceNameResult.FirstChild.Data
-			}
-			if raceTrackResult.FirstChild != nil {
-				newEvent[3] = raceTrackResult.FirstChild.Data
-			}
+			name, location := GetNameAndLocation(raceResult, raceNameSel, raceTrackSel)
+			newEvent[0] = name
+			newEvent[3] = location
 		}
 		// date and time
 		lightsOutResult := Query(row, lightsOutSel)
 		if lightsOutResult != nil {
-			dateTimeResult := Query(lightsOutResult, dateTimeSel)
-			//if race in past, there's no span (just winner's name)
-			if dateTimeResult != nil && dateTimeResult.FirstChild != nil {
-				rawDateTimeStr := dateTimeResult.FirstChild.Data
-				fullDateAndTime := strings.Split(rawDateTimeStr, " - ")
-				fullDate := fullDateAndTime[0]
-				monthAndDay := strings.Split(fullDate, " ")
-				month := monthAndDay[0]
-				formattedMonth := MonthMap[month]
-				day := monthAndDay[1]
-				formattedDay := day
-				if len(day) == 1 {
-					formattedDay = "0" + formattedDay
-				}
-				eventTime := fullDateAndTime[1]
-				hourAndPeriod := strings.Split(eventTime, " ")
-				hour := hourAndPeriod[0]
-				period := hourAndPeriod[1]
-				formattedPeriod := strings.ToUpper(period)
-				year := strconv.Itoa(time.Now().Year())
-				formattedDate := formattedMonth + "/" + formattedDay + "/" + year
-				formattedTime := hour + " " + formattedPeriod
-				newEvent[1] = formattedDate
+			date, eventTime := GetDateAndTime(lightsOutResult, dateTimeSel)
+				newEvent[1] = date
 				newEvent[2] = formattedTime
 			}
 		}
@@ -136,6 +182,7 @@ ParseRows:
 		}
 		events = append(events, newEvent)
 	}
+	//STEP FOUR: WRITE TO CSV 
 	dir := "csv"
 	//GIVE ME DEM WRITE PERMS YA DIG?
 	os.Mkdir(dir, 0777)
@@ -160,6 +207,5 @@ ParseRows:
 	writer.Flush()
 	check(writer.Error())
 	fmt.Printf("file %s created\n", fileName)
-	fmt.Println("import it into your google calendar")
-	fmt.Println("https://go.dev/doc/tutorial/add-a-test")
+	fmt.Println("import it into your google calendar, son")
 }
